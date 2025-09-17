9cd0042469df8043b65724c3aef0020be51ce50f
Ritesh Chawla <riteshchawla@gmail.com>
2025-09-16 22:50:33 -0400

    Updated app.test.js

--- sample-app/app.test.js
+++ sample-app/app.test.js
@@ -9,19 +9,5 @@ describe('Test the app', () => {
     expect(response.text).toBe('Hello, World!');          
   });
 
-  test('Get /name/Bob should return Hello, Bob!', async () => {
-    const response = await request(app).get('/name/Bob');
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello, Bob!');
-  });
-
-  const maliciousUrl = '/name/%3Cscript%3Ealert("hi")%3C%2Fscript%3E';
-  const sanitizedHtml = 'Hello, &lt;script&gt;alert(&#34;hi&#34;)&lt;/script&gt;!'
-
-  test('Get /name should sanitize its input', async () => {
-    const response = await request(app).get(maliciousUrl);
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe(sanitizedHtml);
-  });
 
 });
